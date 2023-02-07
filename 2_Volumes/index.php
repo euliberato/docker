b8ab0d40c25333b94cff5dd28f49f3723af3f955
euliberato <ruanlib.2003@gmail.com>
2023-02-07 18:32:41 -0300

    Aprendendo sobre network

--- 2_Volumes/index.php
+++ 2_Volumes/index.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Escreva sua mensagem</h1>
+    <h1>Escreva sua mensagem | Bind Mount</h1>
     <form action="process.php" method="POST">
         <input type="text" name="message" id="message">
         <input type="submit" value="Enviar Mensagem">
